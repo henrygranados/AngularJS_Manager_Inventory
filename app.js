e4195a8112dca09d7e65e668e903b9b540d01b0e
Henry Granados <henrygranados03@gmail.com>
2015-12-20 13:39:47 -0700

    fixed bug edtting a customer and did some clean up code

--- app.js
+++ app.js
@@ -31,35 +31,25 @@ App.controller('mainController', function($scope, $modal, $log, $filter) {
      $scope.items[0].lateLoader = ' ';  
     });
   }, 1000);
-  /*
-  This function adds a new customer
-  */
-   $scope.addPerson = function(){
-    var customer = {
-        name: $scope.name,
-        Lastname: $scope.Lastname,
-        age: $scope.age,
-    };
-    
-    $scope.People.push(customer);
-  };
+ 
    /*
   This function removes a customer
    */
-   $scope.removePerson = function(index){
+   $scope.removePerson = function(index, searchPerson){
     if(confirm("Are you sure to remove Customer")){
         angular.forEach($scope.People, function(value, key) {
-            if (value.name === index){
+            if (value.name == index){
                 $scope.People.splice(key, 1);
+                $scope.searchPerson = null;
             }
         });
     }
 }; 
-var modalTemplate= '<div class="modal-header">   <a class="close" data-dismiss="modal" ng-click="cancel()"><i class="fa fa-times-circle-o" style="margin:10px;color:black;font-size:35px;"></i></a><h1>.</h1></div><div class="modal-body"><form >' +
+var modalTemplate= '<div class="modal-header">   <a class="close" data-dismiss="modal" ng-click="cancel()"><i class="fa fa-times-circle-o" style="margin:10px;color:black;font-size:35px;"></i></a><h1>Customer</h1></div><div class="modal-body"><form >' +
         ' <label class="col-sm-3 control-label no-padding-right ng-binding">NAME:</label><input style = "width:200px;"type="text" class="form-control ng-scope ng-pristine ng-valid" ng-model="person.name"></br>' +
         ' <label class="col-sm-3 control-label no-padding-right ng-binding">LASTNAME:</label><input style = "width:200px;" type="text" class="form-control ng-scope ng-pristine ng-valid" ng-model="person.Lastname"></br>' +
         ' <label class="col-sm-3 control-label no-padding-right ng-binding">AGE:</label><input style = "width:200px;" type="number"class="form-control ng-scope ng-pristine ng-valid" ng-model="person.age"></br>' +
-        ' <button id = "myid" type="button" class="btn btn-success" ng-click="add()"><i class="ace-icon fa fa-check"></i>Save</button>' +
+        ' <button id = "myid" type="button" class="btn btn-success" ng-click="add(person.name)"><i class="ace-icon fa fa-check"></i>Save</button>' +
         '  <button type="reset" class="btn ">Clear</button>' +
         ' </form>' +
         '</div>' +
@@ -80,6 +70,14 @@ var modalTemplate= '<div class="modal-header">   <a class="close" data-dismiss="
       }
     });
     modalInstance.result.then(function (newPerson) {
+      
+      angular.forEach($scope.People, function(value, key) {
+            if (value.name == newPerson.name){
+                $scope.People.splice(key, 1);
+              //$scope.People.splice(-1, 1);
+            }
+          });
+
       $scope.People.push(newPerson);
     });
   };
@@ -97,8 +95,18 @@ var ModalInstanceCtrl = function($scope, $modalInstance, person) {
     $modalInstance.dismiss('cancel');
   };
 
-  $scope.add = function() {
+  $scope.add = function(newItem) {
+    if(newItem == null || newItem == ''){
+
+          angular.forEach($scope.People, function(value, key) {
+            if (value.name == newItem){
+              $scope.People.splice(key, 1);
+            }
+          }); 
+        }
+      else{
     $modalInstance.close($scope.person);
+  }
   };
 }; 
    
